6ab8e140c4c0f5c9c1ee88882cd58bbdb4433e3f
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-06-19 18:15:42 +0200

    Modified constant

--- mobile-generator/src/ressource/constants.ts
+++ mobile-generator/src/ressource/constants.ts
@@ -1,29 +1,41 @@
 import { SdkVersion } from './sdk_version'
-import { PlatformType } from './enum';
+import { PlatformType, TemplateType } from './enum';
 
-export const SDK_VERSION = {
-    Android: [
-        new SdkVersion(21),
-        new SdkVersion(22),
-        new SdkVersion(23),
-        new SdkVersion(24),
-        new SdkVersion(25),
-        new SdkVersion(26),
-        new SdkVersion(27),
-        new SdkVersion(28),
-        new SdkVersion(29),
-    ],
-    iOS: [
-        new SdkVersion(8,4),
-        new SdkVersion(9,3),
-        new SdkVersion(10,3),
-        new SdkVersion(12,3),
-    ],
-    Flutter: []
+export const GLOBAL = {
+    Android: {
+        sdk_version: [
+            new SdkVersion(21),
+            new SdkVersion(22),
+            new SdkVersion(23),
+            new SdkVersion(24),
+            new SdkVersion(25),
+            new SdkVersion(26),
+            new SdkVersion(27),
+            new SdkVersion(28),
+            new SdkVersion(29),
+        ],
+        template: [
+            TemplateType.Drawer,
+            TemplateType.SingleView,
+            TemplateType.Tabbar
+        ],
+        advised_min_sdk: new SdkVersion(21),
+        advised_target_sdk: new SdkVersion(29),
+
+    },
+    iOS: {
+        sdk_version: [
+            new SdkVersion(8, 4),
+            new SdkVersion(9, 3),
+            new SdkVersion(10, 3),
+            new SdkVersion(12, 3),
+        ],
+        template: [
+            TemplateType.SingleView,
+            TemplateType.Tabbar
+        ],
+        advised_min_sdk: ['sdk_version'][-3],
+        advised_target_sdk: ['sdk_version'][-1],
+    },
+    Flutter: {}
 }
-
-export const ADVISED_MIN_ANDROID = new SdkVersion(21);
-export const ADVISED_TARGET_ANDROID = new SdkVersion(29);
-
-export const ADVISED_MIN_IOS = SDK_VERSION[PlatformType.IOS][-3];
-export const ADVISED_TARGET_IOS = SDK_VERSION[PlatformType.IOS][-1];
