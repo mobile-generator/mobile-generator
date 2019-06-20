0f2a67253fb7879d54feb8f1eb374a6f36eae7b5
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-06-20 16:31:44 +0200

    chore : tslint global-configuration.ts

--- mobile-generator/src/main/configuration/global-configuration.ts
+++ mobile-generator/src/main/configuration/global-configuration.ts
@@ -1,10 +1,10 @@
-import { PlatformType } from './enum';
-import { ConstantPlatformConfiguration, IConstantPlatformConfiguration } from './constant-platform-configuration';
+import { PlatformType } from './enum'
+import { ConstantPlatformConfiguration } from './constant-platform-configuration'
 
 export class GlobalConfiguration {
-    platform = Object.keys(PlatformType).map(key => ConstantPlatformConfiguration.fromPlatformType((<any>PlatformType)[key]));
+    platform = Object.keys(PlatformType).map(key => ConstantPlatformConfiguration.fromPlatformType((PlatformType as any)[key]))
     getPlatformConfiguration(platform: PlatformType) {
         // this.platform[0] will never be called since all PlatformType exist in this.platform
-        return this.platform.find(config => config.platform == platform) || this.platform[0];
+        return this.platform.find(config => config.platform === platform) || this.platform[0]
     }
 }
