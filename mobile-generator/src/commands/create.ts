965396dc5a289fdb75fb86ff9cd5d611f35072f6
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-06-20 15:26:16 +0200

    Enhanced create command

--- mobile-generator/src/commands/create.ts
+++ mobile-generator/src/commands/create.ts
@@ -1,12 +1,13 @@
 import { Command, flags } from '@oclif/command'
-import cli from 'cli-ux'
 import * as inquirer from 'inquirer'
-import { GLOBAL } from '../ressource/constants'
-import { PlatformType } from '../ressource/enum';
-import { Configuration } from '../ressource/ configuration';
+import { PlatformType } from '../main/enum';
+import { Configuration } from '../main/configuration';
+import { MobilePlatformConfiguration } from '../main/mobile_platform_configuration';
+import { GlobalConfiguration } from '../main/global_configuration';
 
 export default class Create extends Command {
   configuration = new Configuration();
+  global_configuration = new GlobalConfiguration();
 
   static description = 'This command is used to create a new template. You need to give the targeted platform and app name'
 
@@ -20,12 +21,15 @@ export default class Create extends Command {
 
   static args = []
 
-
-
-  async run() {
-    const { args, flags } = this.parse(Create)
-
+  async getCommonConfig() {
     let commonResponses: any = await inquirer.prompt([
+      // Prompt for platform target
+      {
+        type: 'list',
+        name: 'platform',
+        message: 'Select targeted platform',
+        choices: Object.keys(PlatformType).map(key => ({ name: key, value: (<any>PlatformType)[key] })),
+      },
       // Prompt for app name
       {
         type: 'input',
@@ -38,65 +42,71 @@ export default class Create extends Command {
           return 'my-app';
         }
       },
-      // Prompt for platform target
-      {
-        type: 'list',
-        name: 'platform',
-        message: 'Select targeted platform',
-        choices: Object.keys(PlatformType).map(key => ({ name: key })),
-      },
       // Prompt for app id
       {
-        type: 'number',
+        type: 'input',
         name: 'app_id',
         message: "What's your app id",
+        validate: function validateAppId(name) {
+          return name !== '';
+        },
         default: function () {
-          return Math.floor(100000 + Math.random() * 900000);
+          return 'com.mycompany.myapp';
         }
       }
     ])
 
     this.configuration.app_name = commonResponses.app_name
     this.configuration.app_id = commonResponses.app_id
-    this.configuration.mobile_platform = commonResponses.platform
+    this.configuration.mobile_platform_configuration = MobilePlatformConfiguration.fromPlatformType(commonResponses.platform)
 
-    if (this.configuration.mobile_platform !== 'flutter') {
+  }
 
+  async getSpecificConfig() {
+    if (this.configuration.mobile_platform_configuration.platform === PlatformType.Flutter) {
+      // Flutter CLI will fill corresponding value
+
+    } else {
       let sdk_target_version: any = await inquirer.prompt([
         {
           type: 'list',
           name: 'value',
-          message: `What's your target ${this.configuration.mobile_platform} SDK version ?`,
-          choices: GLOBAL[this.configuration.mobile_platform]['sdk_version'].map(version => ({name: version.toString(), value: version })),
+          message: `What's your target ${this.configuration.mobile_platform_configuration.platform} SDK version ?`,
+          choices: this.global_configuration
+            .getPlatformConfiguration(this.configuration.mobile_platform_configuration.platform)
+            .sdk_version
+            .map(version => ({ name: version.toString(), value: version })),
         }
       ])
 
-      this.configuration.sdk_target_version = sdk_target_version.value
+      this.configuration.mobile_platform_configuration.sdk_target_version = sdk_target_version.value
 
       let sdk_min_version: any = await inquirer.prompt([
         {
           type: 'list',
           name: 'value',
-          message: `What's your min ${this.configuration.mobile_platform} SDK version ?`,
-          choices: GLOBAL[this.configuration.mobile_platform]['sdk_version']
-            .filter(version => version.compare(this.configuration.sdk_target_version) <= 0)
-            .map(version => ({name: version.toString(), value: version })),
+          message: `What's your min ${this.configuration.mobile_platform_configuration.platform} SDK version ?`,
+          choices: this.global_configuration
+            .getPlatformConfiguration(this.configuration.mobile_platform_configuration.platform)
+            .sdk_version
+            .filter(version => version.compare(this.configuration.mobile_platform_configuration.sdk_target_version) <= 0)
+            .map(version => ({ name: version.toString(), value: version })),
         }
       ])
 
-      this.configuration.sdk_min_version = sdk_min_version.value
+      this.configuration.mobile_platform_configuration.sdk_min_version = sdk_min_version.value
 
       let template: any = await inquirer.prompt([
         {
           type: 'list',
           name: 'value',
           message: `What kind of application template would you use ?`,
-          choices: GLOBAL[this.configuration.mobile_platform]['template']
-            .map(template => ({name: template.toString(), value: template })),
+          choices: this.configuration.mobile_platform_configuration.template
+            .map(template => ({ name: template.toString(), value: template })),
         }
       ])
 
-      this.configuration.template = template.value
+      this.configuration.mobile_platform_configuration.template = template.value
 
       let internet_permission: any = await inquirer.prompt([
         {
@@ -108,11 +118,18 @@ export default class Create extends Command {
 
       this.configuration.internet_permission = internet_permission.value
 
-    } else {
-      // Flutter CLI will fill corresponding value
     }
+  }
 
-    this.log(`Configuration ${this.configuration.toSring()}`)
+
+  async run() {
+    const { args, flags } = this.parse(Create)
+
+    this.getCommonConfig()
+
+    this.getSpecificConfig()
+
+    this.log(`Configuration ${this.configuration.toString()}`)
   }
 }
 
