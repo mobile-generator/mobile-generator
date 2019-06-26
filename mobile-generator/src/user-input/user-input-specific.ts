0371319c747b687651cf97abd4cadea982e8f96e
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-06-26 15:15:03 +0200

    Added advised hint when choosing sdk

--- mobile-generator/src/user-input/user-input-specific.ts
+++ mobile-generator/src/user-input/user-input-specific.ts
@@ -11,30 +11,61 @@ export async function specificConfigForm(configuration: Configuration) {
         // Flutter CLI will fill corresponding value
 
     } else {
+        const advised_target_version_index = global_configuration
+            .getPlatformConfiguration(configuration.platform_configuration.platform)
+            .sdk_version
+            .map(version => {
+                return version.compare(
+                    global_configuration
+                        .getPlatformConfiguration(configuration.platform_configuration.platform)
+                        .sdk_target_version)
+            })
+            .indexOf(0)
+
+        const target_version_list = global_configuration
+            .getPlatformConfiguration(configuration.platform_configuration.platform)
+            .sdk_version
+            .map(version => ({ name: version.toString(), value: version }))
+
+        target_version_list[advised_target_version_index].name += ' (advised)'
+
         let sdk_target_version: any = await inquirer.prompt([
             {
                 type: 'list',
                 name: 'value',
                 message: `What's your target ${configuration.platform_configuration.platform} SDK version ?`,
-                choices: global_configuration
-                    .getPlatformConfiguration(configuration.platform_configuration.platform)
-                    .sdk_version
-                    .map(version => ({ name: version.toString(), value: version })),
+                default: advised_target_version_index,
+                choices: target_version_list,
             }
         ])
 
         configuration.platform_configuration.sdk_target_version = sdk_target_version.value
+
+        const advised_min_version_index = global_configuration
+            .getPlatformConfiguration(configuration.platform_configuration.platform)
+            .sdk_version
+            .map(version => {
+                return version.compare(
+                    global_configuration
+                        .getPlatformConfiguration(configuration.platform_configuration.platform)
+                        .sdk_min_version)
+            })
+            .indexOf(0)
+
+        const min_version_list = global_configuration
+            .getPlatformConfiguration(configuration.platform_configuration.platform)
+            .sdk_version
+            .map(version => ({ name: version.toString(), value: version }))
+
+        min_version_list[advised_min_version_index].name += ' (advised)'
 
         let sdk_min_version: any = await inquirer.prompt([
             {
                 type: 'list',
                 name: 'value',
                 message: `What's your min ${configuration.platform_configuration.platform} SDK version ?`,
-                choices: global_configuration
-                    .getPlatformConfiguration(configuration.platform_configuration.platform)
-                    .sdk_version
-                    .filter(version => version.compare(configuration.platform_configuration.sdk_target_version) <= 0)
-                    .map(version => ({ name: version.toString(), value: version })),
+                default: advised_min_version_index,
+                choices: min_version_list,
             }
         ])
 
