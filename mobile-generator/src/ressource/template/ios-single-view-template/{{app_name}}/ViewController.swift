9e65e8f3582426892521efc6c4c7bcea7412a189
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-06-26 11:04:24 +0200

    Added template

---
+++ mobile-generator/src/ressource/template/ios-single-view-template/{{app_name}}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{app_name}}
+//
+//  Created by Franck Clement on 20/06/2019.
+//  Copyright © 2019 ippon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ mobile-generator/src/ressource/template/ios-tabbar-template/{{app_name}}/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  {{app_name}}
+//
+//  Created by Franck Clement on 21/06/2019.
+//  Copyright © 2019 ippon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
