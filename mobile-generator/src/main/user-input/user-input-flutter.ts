0aad1f9aeb5887c83afe03e201e86612143252b7
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-04 10:03:25 +0200

    Checking Flutter install

--- mobile-generator/src/main/user-input/user-input-flutter.ts
+++ mobile-generator/src/main/user-input/user-input-flutter.ts
@@ -1,3 +1,7 @@
+import * as inquirer from 'inquirer'
+
+import { flutterIsInstalled, installFlutter } from '../utils/flutter-cli';
+
 /**
  * flutterConfigForm
  * @param configuration user configuration
@@ -13,5 +17,35 @@ export async function flutterConfigForm() {
      * Ask user if we can install it 
      * use Flutter CLI to create Flutter project
      */
+
+    if (!flutterIsInstalled()) {
+
+    }
+
+    return true
+}
+
+
+/**
+ * flutterAskForInstallation
+ * @param configuration user configuration
+ * @returns It will returns a promise which resolve as true if there are no errors
+ * otherwise it will reject it with the error
+ * @summary It will ask user to install Flutter
+ */
+export async function flutterAskForInstallation() {
+    // Prompt for platform target
+    let installation: any = await inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'value',
+            message: 'Do you want to install Flutter ?',
+        }
+    ])
+
+    if (installation.value) {
+        installFlutter()
+    }
+
     return true
 }
