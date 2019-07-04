777aa97e055c57de9cba5935d877523614e3bb26
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-04 15:09:02 +0200

    Renamed file

---
+++ mobile-generator/src/main/utils/flutter-utils.ts
@@ -0,0 +1,41 @@
+const chalkPipe = require('chalk-pipe')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+import { lookpath } from 'lookpath'
+
+import { Configuration } from '../configuration/configuration'
+
+import { Loader } from './loader'
+import { stringToPackageNameFormat } from './string-utils'
+
+/**
+ * flutterIsInstalled
+ * @returns It will returns a promise which resolve as true if flutter is installed
+ * otherwise it will resolve as false
+ * @summary It will check if flutter is installed
+ */
+export async function flutterIsInstalled() {
+    const path = await lookpath('flutter')
+
+    // path will be undefined if `flutter` is not found in path
+    if (path) {
+        return true
+    }
+    return false
+}
+
+export async function createFlutterProject(configuration: Configuration, directory: string) {
+    const loader = Loader.startWithMessage(' Creating Flutter project')
+    return new Promise<string>(async function (resolve, reject) {
+        const command = `flutter create -i swift -a kotlin -t app --org ${configuration.app_id} --project-name ${stringToPackageNameFormat(configuration.app_name)} ${directory}`
+        console.log('command: ' + command)
+        const { stderr } = await exec(command)
+        if (stderr) {
+            reject(stderr)
+        } else {
+            loader.stop(chalkPipe('green.bold')('✓ Installation done!\n'))
+            resolve('')
+        }
+    })
+}
