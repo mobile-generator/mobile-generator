1c2c942a926c5d583ea58676286b89e8ca8bf918
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-05 17:59:13 +0200

    Added blueprint command

---
+++ mobile-generator/src/commands/blueprint.ts
@@ -0,0 +1,34 @@
+import { Command, flags } from '@oclif/command'
+
+/**
+ * This class represent the create command.
+ * This command is available through the CLI
+ */
+export default class Blueprint extends Command {
+  // Description of the command
+  static description = 'This command is used add or remove a Blueprint'
+
+  // Usage example
+  static examples = [
+    '$ mobile-generator add mobile-generator-my-blueprint',
+    '$ mobile-generator remove mobile-generator-my-blueprint',
+  ]
+
+  // Flags available
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    force: flags.boolean({ char: 'f', description: 'This will overwrite the blueprint if it already exists' })
+  }
+
+  // Args needed
+  static args = [
+    { name: 'operation', description: 'Add or remove a blueprint', options: ['add', 'remove'], required: true },
+    { name: 'path', description: 'path to the blueprint', required: true },
+  ]
+
+  async run() {
+    const { args, flags } = this.parse(Blueprint)
+
+  }
+
+}
